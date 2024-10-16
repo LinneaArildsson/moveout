71f2da8aa7f1421c3fe861525bd9440d859d7e64
Linnéa Arildsson <arildsson31@gmail.com>
2024-10-16 15:14:39 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,14 @@ app.use((req, res, next) => {
 app.use('/labels', labelRoutes)
 app.use('/user', userRoutes)
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back index.html.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
 //Connect to database
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
