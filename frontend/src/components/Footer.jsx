10a2ae3fc2c5c4407736f48d5150e7df6a9047f3
Linnéa Arildsson <arildsson31@gmail.com>
2024-10-16 18:17:17 +0200

    ref

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="footer-content">
         <p>&copy; {new Date().getFullYear()} MoveOut. All rights reserved.</p>
         <p>Uicons by <a href="https://www.flaticon.com/uicons">Flaticon</a></p>
-        <p>Icon made by Freepik from www.flaticon.com</p>
+        <p>Icon made by Freepik from <a href="https://www.flaticon.com/authors/freepik">Freepik</a></p>
       </div>
     </footer>
   );
