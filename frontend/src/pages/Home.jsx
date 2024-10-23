bb73d536d9cb039d13502afe446571dda6b33ff5
Linnéa Arildsson <arildsson31@gmail.com>
2024-10-23 19:25:26 +0200

    test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -30,7 +30,6 @@ export default function Home () {
           },
         }
       );
-      fetchUsers(); // Re-fetch the users to get the updated status
     } catch (error) {
       console.error('Failed to update active status:', error);
       setError('Failed to update active status');
